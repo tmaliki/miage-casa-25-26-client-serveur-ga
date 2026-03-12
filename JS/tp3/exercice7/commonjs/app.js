25ce1ae3fae60c72b5e579faebc472aa6f5eaf88
tmaliki <maliki.tcherou1@gmail.com>
2026-03-12 12:46:00 +0000

    TP3 exercise 6 done, start exercise 7

---
+++ JS/tp3/exercice7/commonjs/app.js
@@ -0,0 +1,10 @@
+const { geometrie } = require("./formules.js");
+
+try {
+    console.log(`Aire du carré de côté 5 : ${geometrie.aireCarre(5)}`);
+    console.log(`Périmètre du carré de côté 5 : ${geometrie.perimetreCarre(5)}`);
+} catch (error) {
+    console.error("Erreur détectée : ", error.message);
+} finally {
+    console.log("Fin des tests.");
+}
