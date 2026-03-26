a914762b608a36c6dd4fae8959f61439e569b202
tmaliki <maliki.tcherou1@gmail.com>
2026-03-26 13:08:44 +0100

    TP3 exercise 9 done and start exercise 10

---
+++ JS/tp3/exercice10/fetch.js
@@ -0,0 +1,6 @@
+// API : liste des todos
+const listTodo = "https://jsonplaceholder.typicode.com/todos";
+
+// API : détail d'un todo
+const findTodo = "https://jsonplaceholder.typicode.com/todos/1";
+
